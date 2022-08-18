7f26cc4440ffb20d1f010274c5c41af3bf15cfd9
Alan Foster <alan-af@outlook.com>
2022-08-18 16:06:02 -0300

    10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
-        <Title headline="Missões" />
-      </div>
+      <>
+        <div data-testid="missions">
+          <Title headline="Missões" />
+        </div>
+        <section>
+          {
+            missions.map((mission, index) => (<MissionCard
+              key={ index }
+              name={ mission.name }
+              year={ mission.year }
+              country={ mission.country }
+              destination={ mission.destination }
+
+            />))
+          }
+        </section>
+      </>
 
     );
   }
