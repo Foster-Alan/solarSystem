a06eb5873fabe486600a130c597ce15e9c3754e8
Alan Foster <alan-af@outlook.com>
2022-08-18 15:29:41 -0300

    6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Title from './Title';
 import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
