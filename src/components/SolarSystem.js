787dd26ebc2049acc7808cd6ef430e84b5143630
Alan Foster <alan-af@outlook.com>
2022-08-18 14:56:44 -0300

    2

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <p>oi</p>
+      </div>
+
+    );
+  }
+}
+
+export default SolarSystem;
