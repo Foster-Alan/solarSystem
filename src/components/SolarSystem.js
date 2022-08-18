f0203bfe15100a94581aa527e2f2f5ab16db1f83
Alan Foster <alan-af@outlook.com>
2022-08-18 15:25:29 -0300

    arrumando prototypes

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -5,11 +5,20 @@ import PlanetCard from './PlanetCard';
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
-        <Title headline="Planetas" />
-        <PlanetCard />
-      </div>
-
+      <>
+        <div data-testid="solar-system">
+          <Title headline="Planetas" />
+        </div>
+        <section>
+          {
+            Planets.map((planetas, index) => (<PlanetCard
+              key={ index }
+              planetName={ planetas.name }
+              planetImage={ planetas.image }
+            />))
+          }
+        </section>
+      </>
     );
   }
 }
